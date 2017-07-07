dfbee59122f0b31c31d94e0f3d0953673fc08aa9
Vinay Sawant <vinay.s@prettysecrets.com>
2017-07-07 12:45:35 +0530

    Added heroku redis

--- server/app.js
+++ server/app.js
@@ -17,9 +17,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(session({
 	store: new RedisStore({
-		host: 'localhost',
-		port: 6379,
+		host: 'ec2-34-226-55-20.compute-1.amazonaws.com',
+		port: 7529,
 		ttl: 900000,
+		pass:'pa49a449f37498f06a82e6458d792694e6768ada5bb1122f83842d13334d7462c'
 		logErrors: true}),
 	secret: 'z{-B";-SQUNF(f2*;e_v2zR4]vg-e:j.',
 	cookie: {maxAge: 180 * 24 * 60 * 60 * 1000}
